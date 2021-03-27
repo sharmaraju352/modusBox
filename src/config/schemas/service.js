bf320da256aba1e05e0a31264affd92e55bfd636
Raju <sharmaraju352@gmail.com>
2021-03-27 21:12:21 +0400

    removed swagger port from env

--- src/config/schemas/service.js
+++ src/config/schemas/service.js
@@ -4,7 +4,6 @@ const defaultSchema = require('config/schemas/default');
 const serviceSchema = defaultSchema.append({
   NODE_ENV: joi.string().valid('test', 'development', 'production').default('development'),
   PORT: joi.number().default(3000),
-  SWAGGER_PORT: joi.number().default(3001),
   BASE_API_URL: joi.string().default(''),
   ALPHAVANTAGE_HOST: joi.string().required(),
   ALPHAVANTAGE_API_KEY: joi.string().required(),
